9ff0c5c0b062bee7fe389be1531bb04179de3b0f
ARMEN ARZUMANYAN <armen.arzumanyan.external@atos.net>
2017-03-03 12:12:21 +0400

    lombok

--- src/main/java/com/project/effects/MessageOrientedBean.java
+++ src/main/java/com/project/effects/MessageOrientedBean.java
@@ -4,40 +4,33 @@ import java.util.Random;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  *
  * @author armdev
  */
 @ManagedBean
+@AllArgsConstructor
+@NoArgsConstructor
 public class MessageOrientedBean {
 
     private final String[] effects = {"blind", "clip", "drop", "explode", "fold", "puff", "slide", "scale", "bounce", "pulsate", "shake", "size"};
+    private final int idx = new Random().nextInt(effects.length);    
+    @Setter
+    @Getter
+    private String secretMessage;   
 
-    private String secretMessage;
-
-    private final int idx = new Random().nextInt(effects.length);
+    @Setter
+    @Getter
     private String effectName = (effects[idx]);
 
     public void sendMessage() {
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Received message:'" + secretMessage + "'"));
     }
 
-    public String getEffectName() {
-        return effectName;
-    }
-
-    public void setEffectName(String effectName) {
-        this.effectName = effectName;
-    }
-    
-
-    public void setSecretMessage(String secretMessage) {
-        this.secretMessage = secretMessage;
-    }
-
-    public String getSecretMessage() {
-        return secretMessage;
-    }
 
 }
